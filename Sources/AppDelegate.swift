33399d9c99c2533376cafae166da0c175ceb535f
tesch <mail@marceltesch.de>
2020-06-05 20:27:12 +0200

    Setup

---
+++ Sources/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+// AppDelegate.swift
+//
+// Created by Marcel Tesch on 2020-06-05.
+// Think different.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+
+    }
+
+    func applicationWillTerminate(_ notification: Notification) {
+
+    }
+
+}
---
+++ Sources/ViewController.swift
@@ -0,0 +1,14 @@
+//
+// ViewController.swift
+//
+// Created by Marcel Tesch on 2020-06-05.
+// Think different.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    
+
+}
